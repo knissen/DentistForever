aed84b8ec41ec9fc59745820930897d9d2a413de
kylenissen <nissenkyle@gmail.com>
2023-08-18 20:59:49 -0600

    Adding stub for Title menu and hooking up buttons

---
+++ DentistForever/Assets/Scripts/TitleMenuManager.cs
@@ -0,0 +1,53 @@
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleMenuManager : MonoBehaviour
+{
+    [SerializeField] private Button _startGameButton;
+    [SerializeField] private Button _quitButton;
+
+    public async void Start()
+    {
+        CancellationTokenSource source = new CancellationTokenSource();
+
+        try
+        {
+            var quitTask = UIUtils.ButtonPressed(_quitButton, QuitPressed);
+            var gameStartTask = UIUtils.ButtonPressed(_startGameButton, StartGame);
+
+            var firstSelected = await Task.WhenAny(quitTask, gameStartTask);
+
+            // Await in case an exception was thrown to propagate it
+            await firstSelected;
+            source.Cancel();
+
+            var thingToDo = firstSelected.Result;
+            await thingToDo();
+        }
+        finally
+        {
+            source.Dispose();
+        }
+    }
+
+    public async Task StartGame()
+    {
+        await SceneManager.LoadSceneAsync("Main");
+    }
+
+    private async Task QuitPressed()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); 
+#endif
+    }
+}
---
+++ DentistForever/Assets/Scripts/UIUtils.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIUtils
+{
+    public static async Task<bool> FadeCanvas(CanvasGroup canvasGroup, float targetAlpha, float time, bool interactable)
+    {
+        float startValue = canvasGroup.alpha;
+        float timer = 0f;
+
+        while (timer < time)
+        {
+            timer += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(startValue, targetAlpha, timer / time);
+
+            await Task.Delay(1);
+        }
+
+        canvasGroup.interactable = interactable;
+        canvasGroup.blocksRaycasts = interactable;
+
+        return true;
+    }
+
+    public static async Task<Task<Button>> SelectButton(CancellationToken ct, params Button[] buttons)
+    {
+        var tasks = buttons.Select((btn) => ButtonPressed(ct, btn));
+        Task<Button> firstFinished = await Task.WhenAny(tasks);
+
+        return firstFinished;
+    }
+
+    private static async Task<Button> ButtonPressed(CancellationToken ct, Button button)
+    {
+        bool isPressed = false;
+        button.onClick.AddListener(() => isPressed = true);
+
+        while (!isPressed && !ct.IsCancellationRequested)
+            await Task.Yield();
+
+        return ct.IsCancellationRequested ? null : button;
+    }
+
+    public static async Task<Func<Task>> ButtonPressed(Button button, Func<Task> actionOnButton)
+    {
+        bool isPressed = false;
+        button.onClick.AddListener(() => isPressed = true);
+
+        while (!isPressed)
+            await Task.Yield();
+
+        return actionOnButton;
+    }
+}
