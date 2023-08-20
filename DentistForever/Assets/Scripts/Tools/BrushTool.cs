bedc85484ba93b0651a4537607a781c8dd946e67
kylenissen <nissenkyle@gmail.com>
2023-08-20 13:43:04 -0600

    Setting up the toothbrush

--- DentistForever/Assets/Scripts/Tools/BrushTool.cs
+++ DentistForever/Assets/Scripts/Tools/BrushTool.cs
@@ -1,4 +1,5 @@
 using FMODUnity;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,12 +11,39 @@ public class BrushTool : MonoBehaviour, IToothTool
     [SerializeField] private int _particlesEmitRate;
     [SerializeField] private float _cleaningRate = 1f;
     [SerializeField] private float _minDeltaToActivate = 0.5f;
+    [SerializeField] private Transform _brushTransform;
 
     [Header("Audio Settings")]
     [SerializeField] private StudioEventEmitter _brushEmitter;
     [SerializeField] private float _audioCoolDownRate = 0.5f;
 
     private float _audioTimer;
+    private float _camX;
+    private float _camY;
+
+    private void Start()
+    {
+        _camX = Camera.main.transform.position.x;
+        _camY = Camera.main.transform.position.y;
+    }
+
+    private void Update()
+    {
+        AlignTool();
+    }
+
+    private void AlignTool()
+    {
+        bool isLeft = transform.position.x < _camX;
+        bool isUp = transform.position.y > _camY;
+
+        Vector3 lookPoint = isLeft ? new Vector3(1, 0, 4) : new Vector3(6, 0, 3);
+        Vector3 rotUpDir = isUp ? Vector3.down : Vector3.up;
+
+        Quaternion rotation = Quaternion.LookRotation(lookPoint - transform.position, rotUpDir);
+
+        _brushTransform.rotation = rotation;
+    }
 
     public void UseTool(Tooth tooth)
     {
